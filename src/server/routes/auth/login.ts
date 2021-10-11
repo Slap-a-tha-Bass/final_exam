1afe9b5faba44c737931abf82440686d2a8e22d2
Corey DeLoach <cdeloach16@gmail.com>
2021-10-11 10:26:20 -0500

    login and register server routes, server, configurepassport

--- src/server/routes/auth/login.ts
+++ src/server/routes/auth/login.ts
@@ -1,9 +1,21 @@
 import * as express from 'express';
+import * as passport from 'passport';
+import * as jwt from 'jsonwebtoken';
+import { ReqUsers } from '../../../../types';
+import { jwtConfig } from '../../config';
 
 const router = express.Router();
 
-router.get('/', async (req, res) => {
-    res.json('World');
+router.post('/', passport.authenticate('local'), async (req: ReqUsers, res) => {
+    try {
+        const token = jwt.sign({ userid: req.user.id, email: req.user.email, role: 'guest'},
+        jwtConfig.secret,
+        {expiresIn: jwtConfig.expires});
+        res.json(token);
+        return;
+    } catch (error) {
+        res.status(500).json({ message: "Error in router", error: error.sqlMessage });
+    }
 });
 
 export default router;
--- src/server/routes/auth/register.ts
+++ src/server/routes/auth/register.ts
@@ -1,9 +1,26 @@
 import * as express from 'express';
-
+import * as passport from 'passport';
+import * as jwt from 'jsonwebtoken';
+import { ReqUsers } from '../../../../types';
+import { jwtConfig } from '../../config';
+import { generateHash } from '../../utils/passwords';
+import { insert_user } from '../../db/queries/users';
 const router = express.Router();
 
-router.get('/', async (req, res) => {
-    res.json('World');
+router.post('/', async (req: ReqUsers, res) => {
+    const { name, email, password} = req.body;
+    try {
+        const hashed = generateHash(password);
+        const newUser = { name, email, password: hashed };
+        const register = await insert_user(newUser);
+        const token = jwt.sign({ userid: register.insertId, email, role: 'guest'},
+        jwtConfig.secret,
+        {expiresIn: jwtConfig.expires});
+        res.json({ register, token });
+        return;
+    } catch (error) {
+        res.status(500).json({ message: "Error in router", error: error.sqlMessage });
+    }
 });
 
 export default router;
