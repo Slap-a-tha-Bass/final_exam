c9aade1093b01a22481aa8a449d3c69153d3db8c
Corey DeLoach <cdeloach16@gmail.com>
2021-10-11 11:22:49 -0500

    home and edit pages working, moving onto UX and double checking all functionality

--- src/client/views/BookDetails.tsx
+++ src/client/views/BookDetails.tsx
@@ -1,10 +1,22 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Link, useParams } from 'react-router-dom';
+import { Books } from '../../../types';
+import BookCard from '../components/BookCard';
+import RootLayout from '../components/RootLayout';
+import { apiService } from '../utils/api-service';
 
 const BookDetails = () => {
+    const { id } = useParams<{ id: string }>();
+    const [book, setBook] = useState<Books['id']>();
+    useEffect(() => {
+        apiService(`/api/books/${id}`)
+            .then(data => setBook(data));
+    }, []);
+
     return (
-        <div>
-            
-        </div>
+        <RootLayout>
+            <BookCard key={id} {...book} isPreview />
+        </RootLayout>
     )
 }
 
--- src/client/views/EditDetails.tsx
+++ src/client/views/EditDetails.tsx
@@ -1,10 +1,78 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useHistory, useParams } from 'react-router';
+import { Link } from 'react-router-dom';
+import { Categories } from '../../../types';
+import RootLayout from '../components/RootLayout';
+import { useForm } from '../hooks/useForm';
+import { apiService } from '../utils/api-service';
 
 const EditDetails = () => {
+    const { id } = useParams<{ id: string }>();
+    const history = useHistory();
+
+    const { values, handleChanges, populate } = useForm();
+
+    const [categories, setCategories] = useState<Categories[]>([]);
+    useEffect(() => {
+        apiService('/api/categories')
+            .then(values => setCategories(values));
+    }, []);
+
+    useEffect(() => {
+        apiService(`/api/books/${id}`)
+            .then(values => populate(values));
+    }, []);
+
+    const handleEdit = (e: React.MouseEvent<HTMLButtonElement>) => {
+        e.preventDefault();
+        apiService(`/api/books/${id}`, 'PUT', { title: values.title, author: values.author, price: values.price, categoryid: values.categoryid })
+            .then(data => {
+                history.push('/books')
+            })
+    }
     return (
-        <div>
-            
-        </div>
+        <RootLayout>
+            <form className="form-group p-2">
+                <label htmlFor="" className="text-info">title</label>
+                <input
+                    name="title"
+                    value={values.title || ''}
+                    onChange={handleChanges}
+                    type="text"
+                    className="form-control" />
+                <label htmlFor="" className="text-info">author</label>
+                <input
+                    name="author"
+                    value={values.author || ''}
+                    onChange={handleChanges}
+                    type="text"
+                    className="form-control" />
+                <label htmlFor="" className="text-info">price</label>
+                <input
+                    name="price"
+                    value={values.price || ''}
+                    onChange={handleChanges}
+                    type="number"
+                    step='.01'
+                    className="form-control" />
+                <select
+                    name="categoryid"
+                    value={values.categoryid}
+                    onChange={handleChanges}
+                    className="form-select mt-2">
+                    <option value="0">Choose Genre</option>
+                    {categories.map((values) => (
+                        <option value={values.id} key={values.id}>
+                            {values.name}
+                        </option>
+                    ))}
+                </select>
+                <div className="d-flex justify-content-center mt-2">
+                    <Link to={`/books/${id}`} className="btn btn-info mx-3">cancel</Link>
+                    <button onClick={handleEdit} className="btn btn-info mx-3">confirm</button>
+                </div>
+            </form>
+        </RootLayout>
     )
 }
 
